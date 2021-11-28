c3e2ea3bda1cfb4e015743ada9f4b82f45ff39bd
ruxandra.o-am <ruxandra.o@adoreme.com>
2021-11-28 14:20:25 +0200

    added typography examples

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className='p-4 container mx-auto'>
       <div className="flex flex-row">
-        <div className="flex flex-col space-y-4 px-2">
+        <div className="flex flex-col space-y-4 px-8">
           <p>Buttons</p>
           <button className='btn btn-primary700 block'> Primary 700 button</button>
           <button className='btn btn-primary600 block'> Primary 600 button</button>
@@ -13,9 +13,9 @@ function App() {
 
         <div className="flex flex-col space-y-4">
           <p>Typography</p>
-          <button className='btn btn-primary700 block'> Primary 700 button</button>
-          <button className='btn btn-primary600 block'> Primary 600 button</button>
-          <button className='btn btn-primary500 block'> Primary 500 button</button>
+          <h1>H1 Heading</h1>
+          <h2>H2 Heading</h2>
+          <h3>H3 Heading</h3>
         </div>
       </div>
     </div>
