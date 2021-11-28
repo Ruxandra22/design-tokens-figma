15cfcd44035eb034cf5e4dfeb4cb5730444c3a2c
ruxandra.o-am <ruxandra.o@adoreme.com>
2021-11-28 14:07:14 +0200

    token conf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Design tokens</h1>
+    <div className='p-4 container mx-auto'>
+      <div className="flex flex-row">
+        <div className="flex flex-col space-y-4 px-2">
+          <p>Buttons</p>
+          <button className='btn btn-primary700 block'> Primary 700 button</button>
+          <button className='btn btn-primary600 block'> Primary 600 button</button>
+          <button className='btn btn-primary500 block'> Primary 500 button</button>
+        </div>
+
+        <div className="flex flex-col space-y-4">
+          <p>Typography</p>
+          <button className='btn btn-primary700 block'> Primary 700 button</button>
+          <button className='btn btn-primary600 block'> Primary 600 button</button>
+          <button className='btn btn-primary500 block'> Primary 500 button</button>
+        </div>
+      </div>
     </div>
   );
 }
